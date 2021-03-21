343dbfe24f21dd5448defc81a0e27bc33663aaa9
Tiago Pascoal <tiago@pascoal.net>
2021-03-21 10:42:35 +0000

    add jest junit reporter

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
   },
-  reporters: ['default',  'jest-sonar'],
+  reporters: ['default',  'jest-sonar','jest-junit'],
   testRegex: '(/__tests__/.*|\\.(test|spec))\\.[tj]sx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
 }
